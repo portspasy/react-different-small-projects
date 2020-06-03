144b316e7224b6b9c4a4c045eb36e18a7f7dbfba
Taulant Culi <t.culi91@gmail.com>
2020-06-03 15:29:13 +0200

    importing 2 components to App.js and passing info to props

--- project00/src/App.js
+++ project00/src/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import faker from "faker";
 
-function App() {
+// ReactJS Components
+import CommentDetail from "./components/comment-detail/comment-detail";
+import ApprovalCard from "./components/approval-card/approval-card";
+
+const App = () => {
   return (
-    <div className="App">
-    
+    <div className="ui container comments">
+      <h3 className="ui dividing header">Comments</h3>
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        firstName={faker.name.firstName()}
+        lastName={faker.name.lastName()}
+        desc={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        firstName={faker.name.firstName()}
+        lastName={faker.name.lastName()}
+        desc={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        firstName={faker.name.firstName()}
+        lastName={faker.name.lastName()}
+        desc={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        firstName={faker.name.firstName()}
+        lastName={faker.name.lastName()}
+        desc={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        firstName={faker.name.firstName()}
+        lastName={faker.name.lastName()}
+        desc={faker.lorem.sentence()}
+      />
+      <ApprovalCard
+        avatar={faker.image.avatar()}
+        firstName={faker.name.firstName()}
+        lastName={faker.name.lastName()}
+        desc={faker.lorem.sentence()}
+      />
     </div>
   );
-}
+};
 
 export default App;
