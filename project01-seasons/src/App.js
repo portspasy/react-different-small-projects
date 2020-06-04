268c634b802559f6d16e10439d472a490cfbc220
Taulant Culi <t.culi91@gmail.com>
2020-06-04 16:19:41 +0200

    creating getSeason function to get month and latitude

--- project01-seasons/src/App.js
+++ project01-seasons/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-
+import SeasonDisplay from "./components/season-display/season-display";
 class App extends React.Component {
-  // initialize state
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: null,
-      lng: null,
-      errorMessage: "",
-    };
+  state = {
+    lat: null,
+    lng: null,
+    errorMessage: "",
+  };
 
+  //componentDidMount is executed after the first render only on the client side. This is where AJAX requests and DOM or state updates should occur.
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         // to update our State!!!
@@ -30,12 +29,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && (this.state.lat || this.state.lng)) {
-      return (
-        <div>
-          <p>Latitude: {this.state.lat}</p>
-          <p>Longitude: {this.state.lng}</p>
-        </div>
-      );
+      return <SeasonDisplay lat={this.state.lat} lng={this.state.lng} />;
     }
 
     return <div>Loading!</div>;
--- project01-seasons/src/components/season-display/season-display.jsx
+++ project01-seasons/src/components/season-display/season-display.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div></div>;
+const getSeason = (lat, month) => {
+  debugger;
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const getMonth = new Date().getMonth();
+  debugger;
+  const season = getSeason(props.lat, getMonth);
+
+  console.log(season);
+
+  return (
+    <div>
+      <h1>SeasonDisplay</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
