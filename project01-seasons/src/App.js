5055c011a59c4f8d45dbf4a13ecf24ebd422a2be
Taulant Culi <t.culi91@gmail.com>
2020-06-04 13:52:07 +0200

    adding contitionally rendering content

--- project01-seasons/src/App.js
+++ project01-seasons/src/App.js
@@ -7,26 +7,38 @@ class App extends React.Component {
     this.state = {
       lat: null,
       lng: null,
+      errorMessage: "",
     };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
+        // to update our State!!!
         this.setState({
           lat: [position.coords.latitude],
           lng: [position.coords.longitude],
         });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   render() {
-    return (
-      <div className="App">
-        <p>Latitude: {this.state.lat}</p>
-        <p>Longitude: {this.state.lng}</p>
-      </div>
-    );
+    if (this.state.errorMessage && !(this.state.lat || this.state.lng)) {
+      return <p>Error: {this.state.errorMessage}</p>;
+    }
+
+    if (!this.state.errorMessage && (this.state.lat || this.state.lng)) {
+      return (
+        <div>
+          <p>Latitude: {this.state.lat}</p>
+          <p>Longitude: {this.state.lng}</p>
+        </div>
+      );
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
