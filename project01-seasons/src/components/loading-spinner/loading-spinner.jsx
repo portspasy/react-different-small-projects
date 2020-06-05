7beddf7bd15c3e4d4c7667f13ac4cf94c4c8c83e
Taulant Culi <t.culi91@gmail.com>
2020-06-05 14:14:57 +0200

    adding a LoadingSpinner.defaultProps to spinner component

--- project01-seasons/src/components/loading-spinner/loading-spinner.jsx
+++ project01-seasons/src/components/loading-spinner/loading-spinner.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./loading-spinner.css";
 
-const LoadingSpinner = () => {
+const LoadingSpinner = (props) => {
   return (
     <div className="loading-spinner">
-      <div className="ui segment" style={{ height: "100vh" }}>
+      <div className="ui segment">
         <div className="ui active dimmer">
-          <div className="ui text loader">Loading</div>
+          <div className="ui text loader">{props.message}</div>
         </div>
         <p></p>
       </div>
     </div>
   );
+};
+
+LoadingSpinner.defaultProps = {
+  message: "Loading...",
 };
 
 export default LoadingSpinner;
